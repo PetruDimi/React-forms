7cafb49eca5244d516eef4e10f032bcbf14fd357
Petru.Dimi <dimitriu_petru@yahoo.com>
2023-09-18 08:46:13 +0300

    deriving a third state from two other states, without usig useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,33 +2,30 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== ''
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
 
   const nameInputBlurHandler = (e) =>{
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
-    }
+    setEnteredNameTouched(true)
   }
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
     setEnteredNameTouched(true)
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnteredNameIsValid(true);
     setEnteredName("");
+    setEnteredNameTouched(false)
   };
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
