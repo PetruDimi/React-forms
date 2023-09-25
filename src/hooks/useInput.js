cb93ad1948444a89c1bd305df75653876f9129eb
Petru.Dimi <dimitriu_petru@yahoo.com>
2023-09-25 18:53:50 +0300

    added custom input hook and form validity, also checkout Formik docs

---
+++ src/hooks/useInput.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const useInput = (validationFunc) =>{
+    const [value, setValue] = useState('')
+    const [isTouched, setIsTouched] = useState(false)
+
+    const valueIsValid = validationFunc(value)
+    const valueHasError = !valueIsValid && isTouched
+
+    const valueChangeHandler = (e) =>{
+        setValue(e.target.value)
+    }
+
+    const inputBlurHandler = () =>{
+        setIsTouched(true)
+    }
+
+    const reset = () =>{
+        setValue('')
+        setIsTouched(false)
+    }
+
+    return {
+        value,
+        valueIsValid,
+        valueHasError,
+        valueChangeHandler,
+        inputBlurHandler,
+        reset
+    }
+}
+
+export default useInput
