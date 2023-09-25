397ad599dde3ef78a83600fbb9d5f9b9f95b6b14
Petru.Dimi <dimitriu_petru@yahoo.com>
2023-09-25 19:30:56 +0300

    something

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 const useInput = (validationFunc) =>{
-<<<<<<< HEAD
     const [value, setValue] = useState('')
     const [isTouched, setIsTouched] = useState(false)
 
@@ -10,17 +9,6 @@ const useInput = (validationFunc) =>{
 
     const valueChangeHandler = (e) =>{
         setValue(e.target.value)
-=======
-    
-    const [enteredValue, setEnteredValue] = useState('')
-    const [isTouched, setIsTouched] = useState(false)
-
-    const isValid = validationFunc(enteredValue)
-    const hasError = !isValid && isTouched
-
-    const valueChangeHandler = (e) =>{
-        setEnteredValue(e.target.value)
->>>>>>> 7920d8a5d954c539e0cafbea0e3d2e3c1dd48073
     }
 
     const inputBlurHandler = () =>{
@@ -28,33 +16,20 @@ const useInput = (validationFunc) =>{
     }
 
     const reset = () =>{
-<<<<<<< HEAD
-        setValue('')
-=======
         setEnteredValue('')
->>>>>>> 7920d8a5d954c539e0cafbea0e3d2e3c1dd48073
         setIsTouched(false)
     }
 
     return {
-<<<<<<< HEAD
         value,
         valueIsValid,
         valueHasError,
-=======
-        enteredValue,
-        isValid,
-        hasError,
->>>>>>> 7920d8a5d954c539e0cafbea0e3d2e3c1dd48073
         valueChangeHandler,
         inputBlurHandler,
         reset
     }
 }
 
-<<<<<<< HEAD
-export default useInput
-=======
 export default useInput
 
->>>>>>> 7920d8a5d954c539e0cafbea0e3d2e3c1dd48073
+
